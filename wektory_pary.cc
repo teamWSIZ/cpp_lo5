9edec5a70b6ba43692ebf0d75a566d9380dbecc1
pmarecki <pmarecki@gmail.com>
2015-01-29 10:49:35 +0100

    Dodane zadanie 2

--- wektory_pary.cc
+++ wektory_pary.cc
@@ -31,4 +31,17 @@ int main() {
   //Proszę wypisać numery uczestników posortowanych w kolejności od tego, który uzyskał
   //najwięcej punktów do tego który uzyskał najmniej.
   
+   //Zadanie2: rozpatrzmy liczby całkowite od 0 do 10 włacznie, na osi liczbowej.
+  // Mówimy, ze przedział [a,b] pokrywa liczby od a do b, w tym sensie,
+  // ze zawiera on wszystkie liczby od a do b włacznie, czyli {a,a+1,...,b-1,b}
+  // Zadany jest zbiór przedziałó w postaci wektora par {a,b}.
+  // Proszę sprawdzić, czy każda z liczb od 0 do 10 włącznie jest pokryta przez 
+  // przynajmniej jeden z podanych przedziałów.
+  //Przykład 1:
+  vector<pair<int, int>> W1 = {{1, 3}, {2, 5}, {1, 8}};
+  //odpowiedź: nie (pozotają niepokryte 0, 9, 10
+  //Przykład 2:
+  vector<pair<int, int>> W2 = {{1, 3}, {4, 5}, {5, 10}};
+  //odpowiedź: tak
+  
 };
