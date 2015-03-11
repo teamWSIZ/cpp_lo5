8672287495adba7a3e285ea9471feb3252e9c940
pmarecki <pmarecki@gmail.com>
2015-03-11 17:01:59 +0100

    Create zadanieKolejkaRazyDwaMinusJeden.cc

---
+++ zadanieKolejkaRazyDwaMinusJeden.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define REP(i,n)  for(int i=0;i<(int)(n);++i)
+#define FOR(i,b,n)  for(int i=b;i<(n);++i)
+#define ALL(c) (c).begin(),(c).end()
+#define SS size()
+#define CLR(a,v) memset((a),(v), sizeof a)
+#define ST first
+#define ND second
+template<typename T, typename U> inline void AMIN(T &x, U y) { if(y < x) x = y; }
+template<typename T, typename U> inline void AMAX(T &x, U y) { if(x < y) x = y; }
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef pair<int,int> pii;
+
+//http://codeforces.com/problemset/problem/520/B
+
+int main() {
+    int st = 15, end = 20;
+    queue<int> Q;
+    int dyst[10000] = {0};
+    Q.push(st);
+    dyst[st] = 1;
+    while (Q.size()>0) {
+        int at = Q.front(); Q.pop();
+        dyst[at] =1;
+        cout << "sprawdzam wezel:" << at << endl;
+//        if (at==end) {
+//            cout << "best=" << dist[at] << endl;
+//            break;
+//        }
+        int nowy1 = at * 2;
+        int nowy2 = at -1;
+        if (nowy1<=100 && dyst[nowy1]==0) Q.push(nowy1);
+        if (nowy2>=0 && dyst[nowy2]==0) Q.push(nowy2);
+    }
+
+}
