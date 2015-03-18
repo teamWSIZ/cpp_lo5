f59ab0c4d09488a0290201752e3b1c2c3c5bba56
pmarecki <pmarecki@gmail.com>
2015-03-18 16:00:46 +0100

    Update zadanieRzadKraje.cc

--- zadanieRzadKraje.cc
+++ zadanieRzadKraje.cc
@@ -6,3 +6,30 @@
 // {1,4} ERR
 // {1,2,1,2} ERR
 // {2} ERR
+
+
+bool czyJestDobry(vector<int> dane) {
+  //nasz kod
+  return true; //or false
+}
+
+void test(vector<int> dane, bool dobryWynik) {
+  bool wynik = czyJestDobry(dane);
+  if (wynik==dobryWynik) {
+    cout << "OK" << endl;
+  } else {
+    for(int i=0; i<dane.size(); i++) {
+      cout << ", " << dane[i];
+    }
+    cout << "poprawny wynik:"<< dobryWynik << " otrzymano:" << wynik << endl;
+  }
+}
+
+//wykorzystanie testera:
+int main() {
+  test({1,2,3},true);
+
+};
+
+
+
