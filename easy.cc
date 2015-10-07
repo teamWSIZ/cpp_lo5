8fe7d98a2d0265833611a0c6a6628973848c28c2
pmarecki <pmarecki@gmail.com>
2015-10-07 16:42:03 +0200

    Create easy.cc

---
+++ easy.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define REP(i,n)  for(int i=0;i<(int)(n);++i)
+#define FOR(i,b,n)  for(int i=b;i<(n);++i)
+#define ALL(c) (c).begin(),(c).end()
+#define SS size()
+#define CLR(a,v) memset((a),(v), sizeof a)
+#define ST first
+#define ND second
+template<typename T, typename U> inline void AMIN(T &x, U y) { if(y < x) x = y; }
+template<typename T, typename U> inline void AMAX(T &x, U y) { if(x < y) x = y; }
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef pair<int,int> pii;
+
+int main() {
+    vi w = {2,3,5,7};
+    for(int i : w) {
+      cout << i << endl;
+    }
+}
