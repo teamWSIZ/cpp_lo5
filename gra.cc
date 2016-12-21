7b266de1bb5014f16cbbc175b2d44665591633c7
WSIZ <teamWSIZ@users.noreply.github.com>
2016-12-21 08:33:17 -0800

    Update gra.cc

--- gra.cc
+++ gra.cc
@@ -29,6 +29,16 @@ public:
 		y = other.y;
 	}
 	
+	void add(Point& p) {
+		x += p.x;
+		y += p.y;
+	}
+	
+	void multiply(double factor) {
+		x *= factor;
+		y *= factor;
+	}
+	
 	string to_string() {
 		string res = "";
 		res += "{x=" + std::to_string(x) + ", y=" + std::to_string(y) + "}";
@@ -42,6 +52,11 @@ public:
 	double angle(Point& p) {
 		return (0.0 + p.x * x + p.y * y)/(len() * p.len());
 	}
+	
+	double dist(Point& p) {
+		Point diff(x - p.x, y - p.y);
+		return diff.len();
+	}
 };
 
 Point rakieta(Point& prak, Point& vrak, Point& psam, Point& vsam) {
@@ -50,6 +65,32 @@ Point rakieta(Point& prak, Point& vrak, Point& psam, Point& vsam) {
 	return noweV;
 }
 
+class Engine {
+private:
+	Point psam;
+	Point vsam;
+	Point prak;
+	Point vrak;
+
+public:
+	void run() {
+		psam = Point(2,2);
+		vsam = Point(-1,0);
+		prak = Point(0,0);
+		vrak = Point(0,2);
+		
+		double t = 0;
+		double dt = 0.1;  //powinno byc 0.001
+		while (t<1) {
+			Point diff = vsam;
+			diff.multiply(dt);
+			psam.add(diff);
+			
+			
+		}
+		
+	}
+};
 
 
 
@@ -59,5 +100,10 @@ int main() {
 	Point y(2,0);
 	cout << x.to_string() << endl;
 	cout << x.angle(y) << endl;
-	
+	Point z(0,0);
+	z = x;
+	z.x = 11;
+	cout << x.to_string() << endl;
+	cout << z.to_string() << endl;
+
 }
