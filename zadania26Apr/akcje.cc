474bbf0a1696fbb813cefbedc5bfcbaa9e2261cf
WSIZ <teamWSIZ@users.noreply.github.com>
2017-04-26 18:36:15 +0200

    Update akcje.cc

--- zadania26Apr/akcje.cc
+++ zadania26Apr/akcje.cc
@@ -2,7 +2,9 @@ Zadanie:
  dane są ceny akcji w postaci ciągu liczb całkowitych. Wiadomo, że jest tylko jedna możliwość zmiany ceny akcji: spadają 
  one o "k" (np. k=3) w dół. W ciągu jednego dnia tylko jedna z akcji zmieni swoją cenę. Obliczyć po ilu 
  dniach najszybciej ceny wszystkich akcji będą równe (lub napisać, że to nie możliwe). 
- 
+
+  Pełna treść zadania: http://codeforces.com/contest/793/problem/A
+  
  Przykład
  k=3, 
  ceny:
