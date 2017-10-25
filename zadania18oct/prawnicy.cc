3a89ac8df1b1cca0ef59b88676350c1977b64db7
Piotr Marecki <pmarecki@gmail.com>
2017-10-25 17:09:30 +0200

    Create prawnicy.cc

---
+++ zadania18oct/prawnicy.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n)  for(int i=0;i<(int)(n);++i)
+#define FOR(i, b, n)  for(int i=b;i<(n);++i)
+#define ALL(c) (c).begin(),(c).end()
+#define PB(c) push_back(c)
+#define SS size()
+#define ST first
+#define ND second
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+
+int main() {
+    int n = 6;
+    int k = 3;
+    vector<pii> w = {{3,8},{4,12},{2,6},{1,10},{5,9},{11,12}};
+    
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
