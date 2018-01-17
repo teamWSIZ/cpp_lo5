2275158a01b9d085e6bd1555af22b96a146c8269
Piotr Marecki <pmarecki@gmail.com>
2018-01-17 17:04:15 +0100

    Update jednokierunkowe.cc

--- grafy/jednokierunkowe.cc
+++ grafy/jednokierunkowe.cc
@@ -8,25 +8,23 @@ using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
-typedef vector<set<i>> vsi;
+typedef set<int> si;
+typedef vector<si> vsi;
 
 template<typename T, typename U> static void AMIN(T &x, U y) { if (y < x) x = y; }
 template<typename T, typename U> static void AMAX(T &x, U y) { if (x < y) x = y; }
 
-http://codeforces.com/contest/915/problem/D
+//http://codeforces.com/contest/915/problem/D
 
-vsi nxt, prv; 
+vsi nxt, prv;
 
 int main() {
-	int n, m; cin >> n >> m; 
-	nxt = vi(n,vi());
-	prv = vi(n,vi());
-	REP(i,m) {
-		int u, v; cin >> u >> v; u--, v--;
-		nxt[u].insert(v);
-		prv[v].insert(u);
-	}
+    int n, m; cin >> n >> m;
+    nxt = vector(n,si());
+    prv = vector(n,si());
+    REP(i,m) {
+        int u, v; cin >> u >> v; u--, v--;
+        nxt[u].insert(v);
+        prv[v].insert(u);
+    }
 }
-
-
-
